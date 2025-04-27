da9f7f7c5bf8860c72becf2e4f6c6ce30252827b
JuanKmilo03 <juancamilobame@ufps.edu.co>
2025-04-27 11:36:14 -0500

    final config

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ DATA_FILE = "/data/notas.txt"
 
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOS"),
+        host=os.getenv("DB_HOST"),
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"), 
         password=os.getenv("DB_PASS"), 
